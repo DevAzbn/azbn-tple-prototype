3e3a888a3c936e08fba3b1d62268e4cf3931cef2
Alexander <alexander.zybin@gmail.com>
2016-09-21 22:50:52 +0300

    201609212250

--- azbn-tple.js
+++ azbn-tple.js
@@ -5,7 +5,7 @@ var fs = require('fs');
 
 var param = {
 	
-}
+};
 
 var pb = {
 	
@@ -14,7 +14,7 @@ var pb = {
 pb.regexp = {
 	base : new RegExp(/\[\[azbntple+\s+tpl="([^"=]+)"+([^\]]+)+\]\]/ig),
 	by_param : new RegExp(/([^"=\s]+="[^"]+")/ig),
-	by_value : new RegExp(/([^"=\s]+)([^"=]+)/ig),
+	by_value : new RegExp(/([^"]+)([^"=]+)/ig),//new RegExp(/([^"=\s]+)([^"=]+)/ig),
 };
 
 pb.getFileName = function(file) {
@@ -38,8 +38,11 @@ pb.getFromCode = function(_code, prm) {
 	code.html = _code.replace(pb.regexp.base, pb.basereplacer);
 	
 	for(var k in prm) {
+		
 		var v = prm[k];
+		
 		code.html = code.html.replace(new RegExp("{{" + k + "}}", "ig"), v);
+		
 	}
 	
 	return code;
@@ -57,7 +60,7 @@ pb.getSnippetCode = function(file, prm_str) {
 	if(fs.existsSync(pb.getFileName(file))) {
 		
 		var file_handle = fs.openSync(pb.getFileName(file), 'r', 0644);
-		code.html = fs.readSync(file_handle, 10485760, null, 'utf8')[0];
+		code.html = fs.readSync(file_handle, 104857600, null, 'utf8')[0];
 		fs.close(file_handle);
 		
 		code.html = code.html.replace(pb.regexp.base, pb.basereplacer);
@@ -70,11 +73,20 @@ pb.getSnippetCode = function(file, prm_str) {
 pb.basereplacer = function(str, p1, p2, offset, s) {
 	var code = pb.getSnippetCode((p1));
 	var prm_str = p2.match(pb.regexp.by_param);
+	
 	for(var i in prm_str) {
+		
 		var s = prm_str[i];
 		var prm = s.match(pb.regexp.by_value);
-		code.html = code.html.replace(new RegExp("{{" + prm[0] + "}}", "ig"), prm[1]);
+		
+		if(typeof prm[1] == 'undefined') {
+			code.html = code.html.replace(new RegExp("{{" + prm[0] + "}}", "ig"), ' ');
+		} else {
+			code.html = code.html.replace(new RegExp("{{" + prm[0] + "}}", "ig"), prm[1]);
+		}
+		
 	}
+	
 	return code.html;
 };
 
@@ -83,9 +95,35 @@ module.exports = function(_param) {
 	var ctrl = this;
 	
 	ctrl.name = 'AzbnTplEngine';
+	
 	var log_name = ctrl.name;
 	
 	param = _param;
+	
+	ctrl.cache = {};
+	if(param.cache.tpls.length) {
+		for(var i = 0; i < param.cache.tpls.length; i++) {
+			
+			var _f = param.cache.tpls[i];
+			
+			fs.readFile(param.part_path + _f, 'utf8', function(err, text) {
+				
+				if(err) {
+					
+					console.log('Error: ' + err);
+					
+				} else {
+					
+					if(text && text != '') {
+						
+						ctrl.cache[_f] = text;
+						
+					}
+				}
+			});
+			
+		}
+	};
 	
 	ctrl.parseStr = function(str, data, cb) {
 		
@@ -101,25 +139,39 @@ module.exports = function(_param) {
 	
 	ctrl.parseFile = function(file, data, cb) {
 		
-		fs.readFile(param.part_path + file, 'utf8', function(err, text) {
+		if(ctrl.cache[file]) {
 			
-			if(err) {
+			ctrl.parseStr(ctrl.cache[file], data, function(_err, res_str) {
 				
-				console.log('Error: ' + err);
+				cb(_err, res_str);
 				
-			} else {
+				//console.log(file);
 				
-				if(text && text != '') {
+			});
+			
+		} else {
+			
+			fs.readFile(param.part_path + file, 'utf8', function(err, text) {
+				
+				if(err) {
 					
-					ctrl.parseStr(text, data, function(_err, res_str) {
-						
-						cb(_err, res_str);
-						
-					});
+					console.log('Error: ' + err);
 					
+				} else {
+					
+					if(text && text != '') {
+						
+						ctrl.parseStr(text, data, function(_err, res_str) {
+							
+							cb(_err, res_str);
+							
+						});
+						
+					}
 				}
-			}
-		});
+			});
+			
+		}
 		
 	};
 	
---
+++ example/index.js
@@ -0,0 +1,36 @@
+/*
+подключение к БД MySQL
+*/
+
+var tple = new require('./../azbn-tple')({
+		part_path : './',
+		cache : {
+			tpls : [
+				'tpl.html',
+			],
+		},
+	})
+;
+
+var data = {
+	recipient : 'devazbn@yandex.ru',
+	html : 'текст!1!!!!!!!!!!11!11',
+	item_id : 16516846,
+};
+//console.log(tple.cache['tpl.html']);
+
+setTimeout(function(){
+	tple.parseFile('tpl.html', data, function(_err, res_str) {
+		
+		if(_err) {
+			
+			console.log('Error: ' + _err);
+			
+		} else {
+			
+			console.log(res_str);
+			
+		}
+		
+	});
+},5000);
